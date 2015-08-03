7534b6983a1489ae47e07a7a23c0f587be9fe156
SensitiveLion <andypassidomo@gmail.com>
2015-08-03 11:07:37 -0400

    pushing host-files into txt document

--- proxy.rb
+++ proxy.rb
@@ -10,7 +10,7 @@ proxy = agent.get("http://www.proxy4free.com/list/webproxy1.html")
 link = proxy.links[count]
 proxy = link.click
 proxy_form = proxy.forms.last
-binding.pry
+
 until proxy_form.q == "" || proxy_form.q == "http://www."
   count += 3
   proxy = agent.get("http://www.proxy4free.com/list/webproxy1.html")
@@ -18,19 +18,28 @@ until proxy_form.q == "" || proxy_form.q == "http://www."
   proxy = link.click
   proxy_form = proxy.forms.last
 end
-binding.pry
+
 files = ["ad_servers", "emd", "exp", "fsa", "grm", "hfs", "hjk", "mmt", "pha",
           "psh", "wrz"]
+
+
+fname = "test.txt"
 
 files.each do |type|
   proxy_form.q = "http://hosts-file.net/#{type}.txt"
   proxy = agent.submit(proxy_form)
   doc = Nokogiri::HTML(open(proxy.uri.to_s))
   doc_text_full = doc.css("p").to_s
-  doc_text = doc_text_full.gsub(/^"<p>.*\\r\\n#\\r\\n/, '')
+  doc_text_parse = doc_text_full.gsub(/^<p>.*\n.*\n.*\n.*\n.*\n.*\n.*\n.*\n.*\n/, '')
+  doc_text = doc_text_parse.gsub(/#\r\n/,'')
   text = doc_text.split("\r\n")
   binding.pry
 end
+  File.open(fname, "w+") do |f|
+    text.each { |element| f.puts(element) }
+  end
+
+
 
 
 
